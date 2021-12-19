9043fe3e966a401ab375e745c89178b5c1ea3165
hkemal <hasankemal96@gmail.com>
2021-12-19 23:27:11 +0300

    Staircase problem solved

---
+++ src/com/hkemal/Staircase.java
@@ -0,0 +1,33 @@
+package com.hkemal;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Staircase {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        ResultStaircase.staircase(n);
+    }
+}
+
+class ResultStaircase {
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+    public static void staircase(int n) {
+        // Write your code here
+        IntStream.range(0, n).forEach(i -> {
+            IntStream.range(0, n - i - 1).forEach(j -> {
+                System.out.print(" ");
+            });
+            IntStream.range(0, i + 1).forEach(j -> {
+                System.out.print("#");
+            });
+            System.out.println();
+        });
+    }
+}
+
