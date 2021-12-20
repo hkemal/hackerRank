400c2c73a95f06aee2142993c31aa4ac5f3ac299
hkemal <hasankemal96@gmail.com>
2021-12-21 00:50:45 +0300

    SimpleArraySum refactored

--- src/com/hkemal/A2_SimpleArraySum.java
+++ src/com/hkemal/A2_SimpleArraySum.java
@@ -19,18 +19,15 @@ public class A2_SimpleArraySum {
 }
 
 class ResultSimpleArraySum {
-
     /*
      * Complete the 'simpleArraySum' function below.
      *
      * The function is expected to return an INTEGER.
      * The function accepts INTEGER_ARRAY ar as parameter.
      */
-
     public static int simpleArraySum(List<Integer> ar) {
         Integer total = ar.stream().reduce(0, (num1, num2) -> num1 + num2);
         return total;
     }
-
 }
 
