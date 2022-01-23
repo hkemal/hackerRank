16c9b1e8d5320d09857d85482337a154146a0f2f
hkemal <hasankemal96@gmail.com>
2022-01-23 21:31:16 +0300

    HurdleRace file name refactored

--- src/com/hkemal/D2_HurdleRace.java
+++ src/com/hkemal/D2_HurdleRace.java
@@ -4,7 +4,6 @@ import java.text.ParseException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.function.IntPredicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
