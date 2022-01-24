e53f28cda5d437e37aa00ea58f5ef94fe7372e95
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-01-24 10:43:46 +0300

    PickingNumbers problem solved

--- src/com/hkemal/C9_PickingNumbers.java
+++ src/com/hkemal/C9_PickingNumbers.java
@@ -26,7 +26,6 @@ class ResultPickingNumbers {
      * The function is expected to return an INTEGER.
      * The function accepts INTEGER_ARRAY a as parameter.
      */
-
     public static int pickingNumbers(List<Integer> a) {
         // Write your code here
         final Long[] max = {0L};
