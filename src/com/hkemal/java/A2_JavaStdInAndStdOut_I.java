5e46dabdbbaf0992640cf8e71e7ef84175ef0e2b
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-09-05 14:46:16 +0300

    Java Stdin and Stdout I problem solved

---
+++ src/com/hkemal/java/A2_JavaStdInAndStdOut_I.java
@@ -0,0 +1,16 @@
+package com.hkemal.java;
+
+import java.util.Scanner;
+
+public class A2_JavaStdInAndStdOut_I {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
