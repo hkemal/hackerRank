44a63910cf4f98b551b693bdf48b40681469446c
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-09-05 16:01:05 +0300

    Java Loops II problem solved

---
+++ src/com/hkemal/java/A7_JavaLoops_II.java
@@ -0,0 +1,22 @@
+package com.hkemal.java;
+
+import java.util.Scanner;
+
+public class A7_JavaLoops_II {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = input.nextInt();
+            int b = input.nextInt();
+            int n = input.nextInt();
+            for (int j = 0; j < n; j++) {
+                double multiple = Math.pow(2, j);
+                int res = (int) (b * multiple);
+                a = a + res;
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+    }
+}
