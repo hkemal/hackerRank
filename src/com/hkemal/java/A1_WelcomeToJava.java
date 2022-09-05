e93bb312dc49022eed3a2ca31875472bd66e18f3
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-09-05 14:40:37 +0300

    WelcomeToJava problem solved

---
+++ src/com/hkemal/java/A1_WelcomeToJava.java
@@ -0,0 +1,8 @@
+package com.hkemal.java;
+
+public class A1_WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
