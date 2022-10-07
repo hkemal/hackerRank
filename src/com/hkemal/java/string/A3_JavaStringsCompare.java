1a2ac03bce657809857951957a52b9724a081c87
hkemal <hasankemal96@gmail.com>
2022-10-07 19:56:40 +0300

    Java Strings Substring Compare

---
+++ src/com/hkemal/java/string/A3_JavaStringsCompare.java
@@ -0,0 +1,29 @@
+package com.hkemal.java.string;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class A3_JavaStringsCompare {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        List<String> substrings = new ArrayList<>();
+        for (int i = 0; i < s.length() - k + 1; i++) {
+            substrings.add(s.substring(i, i + k));
+        }
+        Collections.sort(substrings);
+        smallest = substrings.get(0);
+        largest = substrings.get(substrings.size() - 1);
+        return smallest + "\n" + largest;
+    }
+}
