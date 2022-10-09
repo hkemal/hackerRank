d7daed351347edb3b5f4dffc63bf14b0f2d389c9
hkemal <hasankemal96@gmail.com>
2022-10-10 00:10:51 +0300

    Java Pattern Syntax Checker

---
+++ src/com/hkemal/java/string/A7_JavaPatternSyntacChecker.java
@@ -0,0 +1,21 @@
+package com.hkemal.java.string;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class A7_JavaPatternSyntacChecker {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String pattern = in.nextLine();
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (Exception e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+        }
+    }
+}
