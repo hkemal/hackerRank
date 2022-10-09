64234a7d4fe51c475f18577c48d0af72afaac169
hkemal <hasankemal96@gmail.com>
2022-10-10 00:18:06 +0300

    Java Regex

---
+++ src/com/hkemal/java/string/A8_JavaRegex.java
@@ -0,0 +1,18 @@
+package com.hkemal.java.string;
+
+import java.util.Scanner;
+
+public class A8_JavaRegex {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+    }
+}
+
+class MyRegex {
+
+    String pattern = "^(([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5])\\.){3}([0-1]?[0-9]?[0-9]?|2[0-4][0-9]|25[0-5]){1}$";
+}
