74c79ee754f3050c2498ea08712309aa1d4ce8b1
kemal <hasankemal.erkaraca@aurorabilisim.com>
2022-10-10 14:24:18 +0300

    Java Exception Handling

---
+++ src/com/hkemal/java/exceptionHandling/A1_JavaTryCatch.java
@@ -0,0 +1,22 @@
+package com.hkemal.java.exceptionHandling;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class A1_JavaTryCatch {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        try {
+            int number1 = input.nextInt();
+            int number2 = input.nextInt();
+            System.out.println(number1 / number2);
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println(inputMismatchException.getClass().getName());
+        } catch (ArithmeticException arithmeticException) {
+            System.out.println(arithmeticException.getClass().getName() + ": / by zero");
+        }
+    }
+
+}
+
+
